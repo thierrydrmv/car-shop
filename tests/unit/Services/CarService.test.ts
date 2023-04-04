c5b818f55cf25137e077c6b46bd58379484e5523
Thierry <varelathierry@gmail.com>
2023-04-04 09:49:00 -0300

    test: testando services car e motorcycle

---
+++ tests/unit/Services/CarService.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+
+const carOutput: Car = new Car({
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  id: '642b0df19506445d7b0bc77b',
+  doorsQty: 4,
+  seatsQty: 5,
+});
+
+describe('Testando o service dos carros', function () {
+  it('Deve adicionar um carro com sucesso', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.create(carInput);
+    expect(result).to.be.deep.equal(carOutput);
+  });
+
+  it('Buscando um carro pelo id com sucesso', async function () {
+    sinon.stub(Model, 'findById').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.getCar('642b0df19506445d7b0bc77b');
+    expect(result).to.be.deep.equal(carOutput);
+  });
+
+  it('Buscando todos os carros', async function () {
+    sinon.stub(Model, 'find').resolves([carOutput]);
+
+    const service = new CarService();
+    const result = await service.getAllCars();
+    expect(result).to.be.deep.equal([carOutput]);
+  });
+  
+  afterEach(function () {
+    sinon.restore();
+  });
+});
---
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+import MotorCycle from '../../../src/Domains/Motorcycle';
+
+const motorcycleOutput: MotorCycle = new MotorCycle({
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  id: '642b1159be4b3a0428baf084',
+  category: 'Street',
+  engineCapacity: 600,
+});
+
+describe('Adicionando uma moto ao banco de dados', function () {
+  it('Deve adicionar uma moto com sucesso', async function () {
+    const motorcycleInput: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    
+    sinon.stub(Model, 'create').resolves(motorcycleOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.create(motorcycleInput);
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Buscando uma moto pelo id com sucesso', async function () {
+    sinon.stub(Model, 'findById').resolves(motorcycleOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.getMotorcycle('642b1159be4b3a0428baf084');
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Buscando todas as motos', async function () {
+    sinon.stub(Model, 'find').resolves([motorcycleOutput]);
+
+    const service = new MotorcycleService();
+    const result = await service.getAllMotorcycles();
+    expect(result).to.be.deep.equal([motorcycleOutput]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
